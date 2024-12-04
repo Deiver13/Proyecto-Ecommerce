04b370bca79c9571ee9f6379f0151c5ee0d70a45
Deiver David Bolaño Mercado <deiberbolano13@gmail.com>
2024-12-04 12:22:24 -0500

    cambios en el código

--- js/carrito.js
+++ js/carrito.js
@@ -7,42 +7,42 @@ document.addEventListener('DOMContentLoaded', () => {
             id: 1,
             nombre: 'Sombrero vueltiao con la bandera de Colombia, 23 vueltas',
             precio: 250000,
-            imagen: 'assets/img/feature_prod_01.jpg',
+            imagen: 'img/feature_prod_01.jpg',
             categoria: 'sombreros'
         },
         {
             id: 2,
             nombre: 'Sombrero vueltiao-machiembriao',
             precio: 150000,
-            imagen: 'assets/img/sombrero4.jpg',
+            imagen: 'img/sombrero4.jpg',
             categoria: 'sombreros'
         },
         {
             id: 3,
             nombre: 'Sombrero vueltiao colombiano 15 vueltas tricolor',
             precio: 250000,
-            imagen: 'assets/img/feature_prod_02.jpg',
+            imagen: 'img/feature_prod_02.jpg',
             categoria: 'sombreros'
         },
         {
             id: 4,
             nombre: 'Mochila wayuu azul 6',
             precio: 120000,
-            imagen: 'assets/img/shop_04.jpg',
+            imagen: 'img/shop_04.jpg',
             categoria: 'mochilas'
         },
         {
             id: 5,
             nombre: 'Bolso en fique',
             precio: 120000,
-            imagen: 'assets/img/shop_03.jpg',
+            imagen: 'img/shop_03.jpg',
             categoria: 'bolsos'
         },
         {
             id: 6,
             nombre: 'Hamaca',
             precio: 120000,
-            imagen: 'assets/img/category_img_02.jpg',
+            imagen: 'img/category_img_02.jpg',
             categoria: 'hamacas'
         }
     ];
